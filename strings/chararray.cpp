d9b9729d23cd95ddf69098ac8f9ba1d958032810
VIGHNESH <vighneshshukla00@gmail.com>
2024-10-04 22:46:47 +0530

    hell yeah

--- strings/chararray.cpp
+++ strings/chararray.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
+void reverse(char name[],int n){
+    int s=0;
+    int e=n-1;
+    while(s<e){
+        swap(name[s++],name[e--]);
+    }
+}
 int main(){
 
 
@@ -8,4 +15,9 @@ int main(){
     cout<<"Enter the number:-";
     cin >> name;
    cout << "Your name is "<< name<<endl;
+   int len = strlen(name);
+   cout<<"Length of the given string is "<<len<<endl;
+   reverse(name,len);
+   cout<<"The string after the reverse is  "<<name<<endl;
+
 }
